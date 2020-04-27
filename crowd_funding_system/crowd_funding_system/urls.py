734880465ec4b52ee38fc20eaa43500a83693d05
Nouran-yehia <nourayehia1994@gmail.com>
2020-04-27 02:51:08 +0200

    FaceBook Api

--- crowd_funding_system/crowd_funding_system/urls.py
+++ crowd_funding_system/crowd_funding_system/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('projects/', include('projects.urls')),
-    path('', homepage_index, name='homepage_index')
+    path('', homepage_index, name='homepage_index'),
+    path('accounts/',include('allauth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
